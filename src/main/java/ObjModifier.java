faf714cd37a703dd8faff244ecceb72cb975e4c4
William Linna <william.linna@gmail.com>
2019-04-22 14:57:51 +0000

    Improve comments in ObjModifier.java

--- src/main/java/ObjModifier.java
+++ src/main/java/ObjModifier.java
@@ -3,10 +3,10 @@ import java.nio.FloatBuffer;
 public class ObjModifier {
     public static void centerVertices(FloatBuffer buf) {
         // We often get 3D models which are not positioned at the origin of the coordinate system. Placing such models
-        // on a 3D map is harder for users. One solution is to reposition the model such that its center is at the
-        // origin of the coordinate system. Your task is to implement such solution.
+        // on a 3D map is harder for users. One solution is to reposition such a model so that its center is at the
+        // origin of the coordinate system. Your task is to implement the centering.
         //
-        // FloatBuffer stores vertices of a mesh in a continuous array of floats (see below)
+        // FloatBuffer stores the vertices of a mesh in a continuous array of floats (see below)
         // [x0, y0, z0, x1, y1, z1, ..., xn, yn, zn]
         // This kind of layout is common in low-level 3D graphics APIs.
         // TODO: Implement your solution here
